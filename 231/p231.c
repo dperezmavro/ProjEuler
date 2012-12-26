31cf15abdd5b0a305714e70b7bcfe380aa1f0ef8
Dionisio Perez Mavrogenis <dpm3g10@ecs.soton.c.uk>
2012-12-26 19:18:48 +0200

    changed the calculations of the binom coef to include gcd

--- 231/p231.c
+++ 231/p231.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 
-double compute_coefficient(int n , int k);
-int fact(int a);
+long double compute_coefficient(int n , int k);
 int compute_prime_factors(double a);
+double getgcd(double n, double k);
 
 int main(int argc, char *argv[] ){
-    
+
     if(argc != 3 || atoi(argv[1]) > atoi(argv[2]) ){
         printf("Usage  : prog k n, with k <= n \n");
         return 1 ;
@@ -16,41 +17,25 @@ int main(int argc, char *argv[] ){
     int n = atoi(argv[2]);
     
     /*gcd(&n, &k);*/
-    double res = compute_coefficient(n,k);
+    long double res = compute_coefficient(n,k);
     
-    printf("Binomial coefficient of (%d,%d) is %.0f \n", n,k,res);
+    printf("Binomial coefficient of (%d,%d) is %.0llf \n", n,k,res);
     return 0;
 }
 
-double compute_coefficient(int n , int k){
-    double res = 0 ;
+long double compute_coefficient(int n , int k){
+     long double res = 0 ;
     if (n == 0 ){
         res =  0 ;
     }else if (k == 0 ){
        res = 1 ;
     }else{    
-        res = n*compute_coefficient(n-1,k-1) / k;
+        double gcd = getgcd(n,k);
+        res = (n*gcd)*compute_coefficient(n-1,k-1) / (k*gcd);
     }
     
-    printf("Binomial coefficient of (%d,%d) is %.0f \n", n,k,res);
+/*    printf("Binomial coefficient of (%d,%d) is %.0llf \n", n,k,res);*/
     return res ;
-}
-
-int fact(int a){
-    if( a < 0 ){
-        printf("Factorial only coputer for a >= 0 , given %d\n",a);
-        exit(1);
-    }else if( a == 0 ){
-        return 1;
-    }else{
-        int i = 1 ;
-        int res = 1 ;
-        for(; i < a+1 ; i++){
-            res = res*i ;
-        }
-
-        return res ;
-    }
 }
 
 int compute_prime_factors(double a){
@@ -65,4 +50,13 @@ int compute_prime_factors(double a){
 
     }
     return 0 ;
+}
+
+double getgcd(double a , double b){
+    double res = fmod(a,b);
+    if( res == 0 ){
+        return b ;
+    }else{
+        return getgcd(b, fmod(a,b));
+    }
 }
