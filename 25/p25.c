5ad55ae30f9935232b3e1635194e3a69ccc5bc57
Dio Perez Mavrogenis <dpm3g10@ecs.soton.ac.uk>
2013-01-14 13:22:44 +0000

    changed p 25

--- 25/p25.c
+++ 25/p25.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include <gmb.h>
+#include <gmp.h>
 
 int main(void){
 	long double a = 1 ;
 	long double b = 1 ;
 	long double next = 2;
 	int count = 2;
-	long double limit = pow(10,3);
+	long double limit = pow(10,999);
 
 	while (next < limit ){
 		next = a + b ;
 		a = b ;
 		b = next ;
 		count++;
-		printf("Found fib %.0f \n",next);
 	}
 
 	printf("The first 1000-digit Fibonacci number is %d \n",count);
