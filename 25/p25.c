3281752426106a4483da30a95f1174ad7e54e637
Dio Perez Mavrogenis <dpm3g10@ecs.soton.ac.uk>
2013-01-15 01:03:44 +0000

    working on gmp implementation of problem 25

--- 25/p25.c
+++ 25/p25.c
@@ -4,17 +4,26 @@
 #include <gmp.h>
 
 int main(void){
-	long double a = 1 ;
-	long double b = 1 ;
-	long double next = 2;
-	int count = 2;
-	long double limit = pow(10,999);
+	mpz_t a  ;
+	mpz_init_set_d(a,1);
+	mpz_t b  ;
+	mpz_init_set_d(b,2);
+	mpz_t next ;
+	mpz_init_set_d(next,0);
 
-	while (next < limit ){
+	int count = 2;
+	mpz_t limit ;
+	mpz_init(limit);
+	mpz_init_set_d(next,2);
+	mpz_ui_pow_ui(limit,10,999);
+
+	while (mpz_cmp(next,limit) <= 0 ){//limit ){
+	/*
 		next = a + b ;
 		a = b ;
 		b = next ;
 		count++;
+	*/
 	}
 
 	printf("The first 1000-digit Fibonacci number is %d \n",count);
