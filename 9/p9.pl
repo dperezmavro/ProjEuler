913f0df34d17f2ab8d5c62db53bd35536834f055
Dionisio Perez-Mavrogenis <dpm3g10@ecs.soton.ac.uk>
2013-09-02 10:53:30 +0300

    added solution to problem 9

--- 9/p9.pl
+++ 9/p9.pl
@@ -3,12 +3,13 @@
 # http://en.wikipedia.org/wiki/Pythagorean_triple
 # we have : 
 # a + b + c = 1000 (1) and a^2 + b^2 = c^2
-# also, by wikipedia, a = m^2 - n^2 , b = 2mn and c = m^2 + n^2
-# so (1) becomes m(m+n) = 500
-# 500 = 2 * 2 * 5 * 5 * 5
+# also, by wikipedia, a = m^2 - n^2 , b = 2mn and c = m^2 + n^2 and m > n
+# so (1) becomes m(m+n) = 500 = 2 * 2 * 5 * 5 * 5
+# therefore 500 = [2*250,4*125,20*25,100*5]
+# since m > n --> m = [5,250], n = [2,100]
 
-foreach $m (reverse(1 .. 250)) {
-	foreach $n ( 1 .. 100 ){
+foreach $m (250 125 25 5) {
+	foreach $n ( 2 4 20 100 ){
 		print get_nums($m, $n) if $m*($n+$m) == 500 and $m > $n;
 	}
 }
@@ -18,5 +19,5 @@ sub get_nums($m, $n){
 	$b = 2*$m*$n ;
 	$c = $m**2 + $n**2 ;
 	$prod = $a * $b * $c ;
-	return "m = $m, n = $n , a = $a , c = $c , b = $b , product is $prod\n";
+	return "m = $m, n = $n , a = $a , c = $c , b = $b , product is a$prod\n";
 }
