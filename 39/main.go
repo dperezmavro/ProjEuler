643d181e3efa13c11a273bc78576ac246fabcaec
Dionisio Perez-Mavrogenis <dio.perez-mavrogenis@cytora.com>
2018-12-25 23:25:18 +0000

    added new solution

---
+++ 39/main.go
@@ -0,0 +1,90 @@
+package main
+
+import "log"
+
+type side struct {
+	length uint
+	square uint
+}
+
+type triangle struct {
+	a, b, c side
+}
+
+type sides struct {
+	b, c       side
+	sumSquared uint
+}
+
+func buildBigSideSquares(upperLimit uint) map[uint]uint {
+	var biggerSideSquares = make(map[uint]uint, upperLimit/2)
+	var i uint
+	for i = 1; i < upperLimit; i++ {
+		biggerSideSquares[i*i] = i
+	}
+
+	return biggerSideSquares
+}
+
+func calculatePairs(p uint) []sides {
+	var i uint
+	var s []sides
+	for i = 1; i < p; i++ {
+		s = append(s, sides{
+			b: side{
+				length: i,
+				square: i * i,
+			},
+			c: side{
+				length: p - i,
+				square: (p - i) * (p - i),
+			},
+			sumSquared: (i * i) + ((p - i) * (p - i)),
+		})
+	}
+	return s
+}
+
+func allPairs(p uint) []sides {
+	var i uint
+	var upperBound = ((p + 1) / 3) * 2
+	var s []sides
+	for i = 2; i < upperBound; i++ {
+		s = append(s, calculatePairs(i)...)
+	}
+	return s
+}
+
+func getSolutions(p uint) []triangle {
+	m := buildBigSideSquares(p)
+	pairs := allPairs(p)
+	var triangles []triangle
+
+	for _, s := range pairs {
+		if m[s.sumSquared] != 0 && s.c.length+s.b.length+m[s.sumSquared] == p {
+			triangles = append(triangles,
+				triangle{
+					b: s.b,
+					c: s.c,
+					a: side{
+						length: m[s.sumSquared],
+					},
+				})
+		}
+	}
+
+	return triangles
+}
+
+func main() {
+	max := 0
+	var i uint
+	for i = 3; i <= 1000; i++ {
+		solutions := getSolutions(i)
+		if len(solutions) > max {
+			log.Printf("new max for %d: %d", i, len(solutions))
+			max = len(solutions)
+		}
+	}
+
+}
---
+++ 39/main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func TestGetSolutions(t *testing.T) {
+	tests := []struct {
+		name      string
+		sideSum   uint
+		solutions uint
+	}{
+		{
+			name:      "3,4,5",
+			sideSum:   12,
+			solutions: 2,
+		},
+		{
+			name:      "{20,48,52}, {24,45,51}, {30,40,50}",
+			sideSum:   120,
+			solutions: 6,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(*testing.T) {
+			var s = uint(len(getSolutions(tt.sideSum)))
+			if tt.solutions != s {
+				t.Errorf("wanted %d, got: %d", tt.solutions, s)
+			}
+		})
+	}
+}
