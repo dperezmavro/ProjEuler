87d40eb00615430a0f3bab721cab9f1ccc1ffed2
Dionisio Perez-Mavrogenis <dio.perez-mavrogenis@cytora.com>
2018-12-25 21:06:00 +0000

    added optimisation

--- 35/main.py
+++ 35/main.py
@@ -17,18 +17,25 @@ def shifts(n):
     res = [n]
     for d in range(1, len(s_n)):
         a = s_n
-        res.append(int(s_n[d:] +s_n[0:d],10))
-
+        num = int(s_n[d:] +s_n[0:d],10)
+        res.append(num)
     return res
 
-primes = primes_bellow(upper)
-circular_primes = []
 
-for p in primes:
-    mutations = shifts(p)
-    in_p = map(lambda x: x in primes, mutations)
-    is_circ = reduce(lambda a,b: a and b , in_p, True)
-    if is_circ:
-        circular_primes.append(p)
+if __name__ == "__main__":
+    primes = primes_bellow(upper)
+    prime_dict = dict(zip(primes, primes))
 
-print(circular_primes, len(circular_primes))
+    circular_primes = []
+
+    for p in primes:
+        mutations = shifts(p)
+        if mutations == []:
+            continue
+        in_p = map(lambda x: x in prime_dict, mutations)
+        is_circ = reduce(lambda a,b: a and b , in_p, True)
+        if is_circ:
+            circular_primes.append(p)
+
+    circular_primes.pop(0) # remove 1
+    print(circular_primes, len(circular_primes))
